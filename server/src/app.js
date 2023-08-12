0d98810886401c9d866acb0ea42b2b92b3f06b81
eswarprasadk <eswarprasadk@turito.com>
2023-08-12 17:48:27 +0530

    static optimized client loading using node and express

--- server/src/app.js
+++ server/src/app.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 //router groups
 const planetsRouter = require("./routes/planets/planets.router");
 
 const app = express();
+
 // middleware to convert requests data to json before passing to route handlers
 app.use(express.json());
+// static site middleware
+app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(cors({ origin: "http://localhost:3000" }));
 // end point handlers
 app.use("/planets", planetsRouter);
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"))
+);
 
 module.exports = app;
