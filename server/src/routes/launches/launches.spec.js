610b1f56e4047245337b401cd2481b82a32ebf29
eswarprasadk <eswarprasadk@turito.com>
2023-08-14 11:49:58 +0530

    JEST package integrated

---
+++ server/src/routes/launches/launches.spec.js
@@ -0,0 +1,11 @@
+describe("get launches api", () => {
+  test("It shouldrespond with 200", () => {
+    const res = 200;
+    expect(res).toBe(200);
+  });
+});
+
+describe("add new launch api", () => {
+  test("It should catch missing properties error", () => {});
+  test("It should catch invalid date error", () => {});
+});
